537e8d2bb6b714399dba5278a94550ff49d8b2ce
Beau Mueller <muellebr@dukes.jmu.edu>
2025-03-12 15:00:16 -0400

    start of class 3/12 v2

--- dht.c
+++ dht.c
@@ -19,6 +19,13 @@
  */
 static int rank;
 
+
+/*
+ * Initialize a new hash table. Returns the current process ID (always zero in
+ * the serial version)
+ *
+ * (In the parallel version, this should spawn the server thread.)
+ */
 int dht_init()
 {
     local_init();
@@ -27,26 +34,62 @@ int dht_init()
     return rank;
 }
 
+
+/*
+ * Save a key-value association. If the key already exists, the assocated value
+ * is changed in the hash table. If the key does not already exist, a new pair
+ * is created with the given value.
+ *
+ * (In the parallel version, this should perform point-to-point MPI
+ * communication as necessary to complete the DHT operation.)
+ */
 void dht_put(const char *key, long value)
 {
     local_put(key, value);
 }
 
+/*
+ * Retrieve a value given a key. If the key is found, the resulting value is
+ * returned. Otherwise, the function should return KEY_NOT_FOUND.
+ *
+ * (In the parallel version, this should perform point-to-point MPI
+ * communication as necessary to complete the DHT operation.)
+ */
 long dht_get(const char *key)
 {
     return local_get(key);
 }
 
+
+/*
+ * Returns the total size of the DHT.
+ *
+ * (In the parallel version, this should perform MPI communication as necessary
+ * to complete the DHT operation.)
+ */
 size_t dht_size()
 {
     return local_size();
 }
 
+
+/*
+ * Synchronize all client processes involved in the DHT. This function should
+ * not return until other all client processes have also called this function.
+ *
+ * (In the parallel version, this should essentially be a global barrier.)
+ */
 void dht_sync()
 {
     // nothing to do in the serial version
 }
 
+
+/*
+ * Dump contents and clean up the hash table.
+ *
+ * (In the parallel version, this should terminate the server thread.)
+ */
 void dht_destroy(FILE *output)
 {
     local_destroy(output);
