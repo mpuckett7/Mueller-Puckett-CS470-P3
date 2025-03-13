724a47dd416ee4c366e3c50205c107befb5c4213
Mason <pucketmg@login02.cluster.cs.jmu.edu>
2025-03-12 20:33:08 -0400

    Push data movement, no func

--- dht.c
+++ dht.c
@@ -19,30 +19,71 @@
  */
 static int my_rank;
 static int nprocs;
+static const int PUT = 0;
+static const int GET = 1;
+static const int SIZE = 2;
+static const int DONE = 3;
 
 pthread_t server;           // server thread
 bool executing = false;     // are we still using the hash table?
-bool server_bool = false;   // condition variable failsafe
-pthread_cond_t server_cond = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t server_lock = PTHREAD_MUTEX_INITIALIZER;
+// bool server_bool = false;   // condition variable failsafe
+// pthread_cond_t server_cond = PTHREAD_COND_INITIALIZER;
+// pthread_mutex_t server_lock = PTHREAD_MUTEX_INITIALIZER;
+
+/*
+ * Struct to hold key value pair 
+ */
+struct pair_t {
+    int instruction;
+    const char *key;
+    long value;
+};
+
+/*
+ * given a key name, return the distributed hash table owner
+ * (uses djb2 algorithm: http://www.cse.yorku.ca/~oz/hash.html)
+ */
+int hash(const char *name)
+{
+    unsigned hash = 5381;
+    while (*name != '\0') {
+        hash = ((hash << 5) + hash) + (unsigned)(*name++);
+    }
+    return hash % nprocs;
+}
 
 /*
  * Server threads wait here
  */
 void *server_func(void *arg) {
     while(executing){
-        while(!server_bool) {
-            printf("im in herebbbbbbbbbb\n");
-            pthread_mutex_lock(&server_lock);
-            pthread_cond_wait(&server_cond, &server_lock);
-            pthread_mutex_unlock(&server_lock);
+        // pthread_mutex_lock(&server_lock);
+        // while(!server_bool) {
+        //     pthread_cond_wait(&server_cond, &server_lock);
+        // }
+        // // do a single task, go back to waiting
+
+        // pthread_mutex_unlock(&server_lock);
+
+        struct pair_t pair;
+
+        MPI_Recv(&pair, sizeof(pair), MPI_BYTE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        if(pair.instruction == PUT){
+
+        }else if(pair.instruction == GET){
+
+        }else if(pair.instruction == SIZE){
+
+        }else if(pair.instruction == DONE){
+            printf("inside of done\n");
+            break;
         }
-        // do a single task, go back to waiting
-        printf("im in hereaaaaaaaa\n");
     }
     printf("i'm about to return\n");
     return NULL;
 }
+
 /*
  * Initialize a new hash table. Returns the current process ID (always zero in
  * the serial version)
@@ -79,7 +120,19 @@ int dht_init()
  */
 void dht_put(const char *key, long value)
 {
-    local_put(key, value);
+    int owner = hash(key);
+
+    if(my_rank == owner){
+        //lock
+        local_put(key, value);
+        //unlock
+    }else{
+        struct pair_t pair;
+        pair.instruction = PUT;
+        pair.key = key;
+        pair.value = value;
+        MPI_Ssend(&pair, sizeof(pair), MPI_BYTE, owner, 0, MPI_COMM_WORLD); 
+    }
 }
 
 /*
@@ -126,14 +179,19 @@ void dht_sync()
  */
 void dht_destroy(FILE *output)
 {
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("post barrier\n");
+
+    struct pair_t pair;
+    pair.instruction = DONE;
+
     executing = false;
-    pthread_cond_signal(&server_cond);
+
+    MPI_Ssend(&pair, sizeof(pair), MPI_BYTE, (my_rank - 1 + nprocs % nprocs), 0, MPI_COMM_WORLD);
+
+    // server_bool = true;
+    // pthread_cond_signal(&server_cond);
     pthread_join(server, NULL);
-    char *filename = (char*)malloc(sizeof(char) * 32);
-    snprintf(filename, 32, "dump-%d.txt", my_rank);
-    FILE *file = fopen(filename, "w");
-    fprintf(file, "this should be in a bunch of files.\n");
-    fclose(file);
     local_destroy(output);
 }
 
